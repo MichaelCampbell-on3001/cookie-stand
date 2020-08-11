f866e4ad624bf4fced2171f119d82342ba3c667f
Michael Campbell <cam3000@gmail.com>
2020-08-11 15:09:25 -0700

    update to javascript and index

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,3 +1,7 @@
+'useStrict'
+console.log('I am a webpage that is being built by a student, so be patient');
+
+var maxArr = [65,24,38,38,16]
 //create seperate JS object literals for each of the 5 shop location that outputs the following to the sales.html file//
 
 //Seattle//
