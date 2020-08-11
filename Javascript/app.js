e592bbcde910e7eeddcbe3a7f47bf3265ee68b73
Michael Campbell <cam3000@gmail.com>
2020-08-11 15:04:21 -0700

    update to the javascript folder

---
+++ Javascript/app.js
@@ -0,0 +1,18 @@
+//create seperate JS object literals for each of the 5 shop location that outputs the following to the sales.html file//
+
+//Seattle//
+
+
+//Tokyo//
+
+
+
+//Dubai//
+
+
+
+//Paris//
+
+
+
+//Lima//
